14024232942aac25dbd56fd41de09e423435a48e
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-03-08 10:19:23 +0100

    Conectando Mongo a los servicios

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -12,13 +12,12 @@ import {
 	Res,
 	// ParseIntPipe,
 } from '@nestjs/common';
-
 import { Response } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import { CreateProductDto, UpdateProductDto } from '../dtos/products.dto';
-import { ProductsService } from '../services/products.service';
+import { ProductsService } from './../services/products.service';
 
 @ApiTags('products')
 @Controller('products')
@@ -32,9 +31,6 @@ export class ProductsController {
 		@Query('offset') offset = 0,
 		@Query('brand') brand: string,
 	) {
-		// return {
-		//   message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
-		// };
 		return this.productsService.findAll();
 	}
 
@@ -45,29 +41,22 @@ export class ProductsController {
 
 	@Get(':productId')
 	@HttpCode(HttpStatus.ACCEPTED)
-	getOne(@Param('productId', ParseIntPipe) productId: number) {
-		// response.status(200).send({
-		//   message: `product ${productId}`,
-		// });
+	getOne(@Param('productId') productId: string) {
 		return this.productsService.findOne(productId);
 	}
 
-	@Post()
-	create(@Body() payload: CreateProductDto) {
-		// return {
-		//   message: 'accion de crear',
-		//   payload,
-		// };
-		return this.productsService.create(payload);
-	}
+	// @Post()
+	// create(@Body() payload: CreateProductDto) {
+	//   return this.productsService.create(payload);
+	// }
 
-	@Put(':id')
-	update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
-		return this.productsService.update(+id, payload);
-	}
+	// @Put(':id')
+	// update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
+	//   return this.productsService.update(+id, payload);
+	// }
 
-	@Delete(':id')
-	delete(@Param('id') id: string) {
-		return this.productsService.remove(+id);
-	}
+	// @Delete(':id')
+	// delete(@Param('id') id: string) {
+	//   return this.productsService.remove(+id);
+	// }
 }
--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -1,60 +1,54 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
-import { Product } from '../entities/product.entity';
-import { CreateProductDto, UpdateProductDto } from '../dtos/products.dto';
+import { Product } from './../entities/product.entity';
+import { CreateProductDto, UpdateProductDto } from './../dtos/products.dto';
 
 @Injectable()
 export class ProductsService {
-	private counterId = 1;
-	private products: Product[] = [
-		{
-			id: 1,
-			name: 'Product 1',
-			description: 'bla bla',
-			price: 122,
-			image: '',
-			stock: 12,
-		},
-	];
+	constructor(
+		@InjectModel(Product.name) private productModel: Model<Product>,
+	) {}
 
 	findAll() {
-		return this.products;
+		return this.productModel.find().exec();
 	}
 
-	findOne(id: number) {
-		const product = this.products.find((item) => item.id === id);
+	async findOne(id: string) {
+		const product = await this.productModel.findById(id).exec();
 		if (!product) {
 			throw new NotFoundException(`Product #${id} not found`);
 		}
 		return product;
 	}
 
-	create(payload: CreateProductDto) {
-		this.counterId = this.counterId + 1;
-		const newProduct = {
-			id: this.counterId,
-			...payload,
-		};
-		this.products.push(newProduct);
-		return newProduct;
-	}
+	// create(data: CreateProductDto) {
+	//   this.counterId = this.counterId + 1;
+	//   const newProduct = {
+	//     id: this.counterId,
+	//     ...data,
+	//   };
+	//   this.products.push(newProduct);
+	//   return newProduct;
+	// }
 
-	update(id: number, payload: UpdateProductDto) {
-		const product = this.findOne(id);
-		const index = this.products.findIndex((item) => item.id === id);
-		this.products[index] = {
-			...product,
-			...payload,
-		};
-		return this.products[index];
-	}
+	// update(id: number, changes: UpdateProductDto) {
+	//   const product = this.findOne(id);
+	//   const index = this.products.findIndex((item) => item.id === id);
+	//   this.products[index] = {
+	//     ...product,
+	//     ...changes,
+	//   };
+	//   return this.products[index];
+	// }
 
-	remove(id: number) {
-		const index = this.products.findIndex((item) => item.id === id);
-		if (index === -1) {
-			throw new NotFoundException(`Product #${id} not found`);
-		}
-		this.products.splice(index, 1);
-		return true;
-	}
+	// remove(id: number) {
+	//   const index = this.products.findIndex((item) => item.id === id);
+	//   if (index === -1) {
+	//     throw new NotFoundException(`Product #${id} not found`);
+	//   }
+	//   this.products.splice(index, 1);
+	//   return true;
+	// }
 }
