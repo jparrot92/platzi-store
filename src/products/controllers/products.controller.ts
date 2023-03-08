160237364ba401a6b1c66b88293f5b2a091c37f2
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-03-08 13:10:34 +0100

    Pipe para mongoid

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -16,6 +16,8 @@ import { Response } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 import { ParseIntPipe } from '../../common/parse-int.pipe';
+import { MongoIdPipe } from './../../common/mongo-id.pipe';
+
 import { CreateProductDto, UpdateProductDto } from '../dtos/products.dto';
 import { ProductsService } from './../services/products.service';
 
@@ -29,7 +31,7 @@ export class ProductsController {
 	getProducts(
 		@Query('limit') limit = 100,
 		@Query('offset') offset = 0,
-		@Query('brand') brand: string,
+		@Query('brand') brand = '',
 	) {
 		return this.productsService.findAll();
 	}
@@ -41,7 +43,7 @@ export class ProductsController {
 
 	@Get(':productId')
 	@HttpCode(HttpStatus.ACCEPTED)
-	getOne(@Param('productId') productId: string) {
+	getOne(@Param('productId', MongoIdPipe) productId: string) {
 		return this.productsService.findOne(productId);
 	}
 
