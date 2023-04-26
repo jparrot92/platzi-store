0adeec982f2410c090281a89ecbfbe54d72cbbc0
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-04-26 18:58:14 +0200

    Introducción a Guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
+import { AuthModule } from './auth/auth.module';
 import config from './config';
 
 @Module({
@@ -28,6 +29,7 @@ import config from './config';
 		UsersModule,
 		ProductsModule,
 		DatabaseModule,
+		AuthModule,
 	],
 	controllers: [AppController],
 	providers: [
