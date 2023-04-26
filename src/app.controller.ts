61fb91bed4d04b528896cfc28a8247164c0dd1f9
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-04-26 21:59:42 +0200

    Usando un decorador

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
+	@Public()
 	@Get()
 	getHello(): string {
 		return this.appService.getHello();
