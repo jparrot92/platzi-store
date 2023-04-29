af6dd5631fc90080e1ce7f8cfc1593ef9806ab38
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2023-04-29 11:56:51 +0200

    Implementando JWT Guard

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -10,10 +10,12 @@ import {
 	HttpStatus,
 	HttpCode,
 	Res,
+	UseGuards,
 	// ParseIntPipe,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import { MongoIdPipe } from './../../common/mongo-id.pipe';
@@ -25,6 +27,7 @@ import {
 } from '../dtos/products.dto';
 import { ProductsService } from './../services/products.service';
 
+@UseGuards(AuthGuard('jwt'))
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
